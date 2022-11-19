9baf3e3d3b9109357bb9dfa684aa24d7d597e68f
Marcelo Silveira Hayden <mshayden.1998@gmail.com>
2022-11-19 14:15:02 -0300

    main e player update
    agora as plataformas são criadas em X segundos. o personagem é iniciado após cinco segundos (temporário).

--- Main.gd
+++ Main.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+var screen_size
+export (PackedScene) var Platform
+
 
 func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func _physics_process(delta):
 	pass
+
+
+func _on_SpawnTimer_timeout():
+	var platform = Platform.instance()
+	add_child(platform)
+	platform.position = Vector2(rand_range(50, screen_size.x), -10) #define a posição inicial da plataforma, em x, randômico, em y, fora da tela.
--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var active = false
+
 
 const MOVE_SPEED = 500
 const JUMP_FORCE = 1000
@@ -16,22 +18,29 @@ func _ready():
 
 
 func _physics_process(delta):
-	var move_dir = 0
-	if Input.is_action_pressed("player_right"):
-		move_dir += 1
-	if Input.is_action_pressed("player_left"):
-		move_dir -= 1
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
-	
-	var grounded = is_on_floor()
-	y_velo += GRAVITY
-	if grounded and Input.is_action_just_pressed("player_jump"):
-		y_velo = -JUMP_FORCE
-	if grounded and y_velo >= 5:
-		y_velo = 5
-	if y_velo > MAX_FALL_SPEED:
-		y_velo = MAX_FALL_SPEED
+	if active == true:
+		var move_dir = 0
+		if Input.is_action_pressed("player_right"):
+			move_dir += 1
+		if Input.is_action_pressed("player_left"):
+			move_dir -= 1
+		move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+		
+		var grounded = is_on_floor()
+		y_velo += GRAVITY
+		if grounded and Input.is_action_just_pressed("player_jump"):
+			y_velo = -JUMP_FORCE
+		if grounded and y_velo >= 5:
+			y_velo = 5
+		if y_velo > MAX_FALL_SPEED:
+			y_velo = MAX_FALL_SPEED
+	else:
+		pass
 
 
 func flip():
 	pass #talvez flipar o sprite em código
+
+
+func _on_FreeToGoAfter_timeout():
+	active = true
