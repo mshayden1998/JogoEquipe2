25b18b6a3a961213619c69dc176e9e943a42b5ce
Marcelo Silveira Hayden <mshayden.1998@gmail.com>
2022-11-26 02:16:39 -0300

    user interface update
    agora é possível trocar o idioma do jogo na tela final. a ui tem um tema agora.

--- FinalScreen.gd
+++ FinalScreen.gd
@@ -1,6 +1,14 @@
 extends Control
 
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_language"):
+		if TranslationServer.get_locale() == "en":
+			TranslationServer.set_locale("pt")
+		else:
+			TranslationServer.set_locale("en")
+
+
 func _on_BtnExit_button_up():
 	get_tree().quit()
 
---
+++ LanguageScreen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_ChoosePortuguese_pressed():
+	pass # Replace with function body.
+
+
+func _on_ChooseEnglish_pressed():
+	pass # Replace with function body.
